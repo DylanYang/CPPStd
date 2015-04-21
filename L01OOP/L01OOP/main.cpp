d108efc9fe2f8d44d1d443f42e6372d393328190
Dylan Yang <dylanyang963@gmail.com>
2015-04-22 00:16:01 +0800

    Create L01OOP
    First program

---
+++ L01OOP/L01OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L01OOP
+//
+//  Created by Dylan Yang on 4/22/15.
+//  Copyright (c) 2015 Dylan Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
