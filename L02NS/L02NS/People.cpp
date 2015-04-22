da4ecf51d2c8746682f86989e262929a3cf574d7
Dylan Yang <dylanyang963@gmail.com>
2015-04-22 13:21:11 +0800

    ini project
    ini project

---
+++ L02NS/L02NS/People.cpp
@@ -0,0 +1,13 @@
+//
+//  People.cpp
+//  L01OOP
+//
+//  Created by Dylan Yang on 4/22/15.
+//  Copyright (c) 2015 Dylan Yang. All rights reserved.
+//
+
+#include "People.h"
+
+void People::sayHello(){
+    printf("Hello");
+}
---
+++ L02NS/L02NS/People.h
@@ -0,0 +1,19 @@
+//
+//  People.h
+//  L01OOP
+//
+//  Created by Dylan Yang on 4/22/15.
+//  Copyright (c) 2015 Dylan Yang. All rights reserved.
+//
+
+#ifndef __L01OOP__People__
+#define __L01OOP__People__
+
+#include <stdio.h>
+
+#endif /* defined(__L01OOP__People__) */
+
+class People{
+public:
+    void sayHello();
+};
